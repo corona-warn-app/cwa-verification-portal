4b0f9fa5c79b9008fd92ca49dd1345a2e0610553
Torsten Egenolf <torsten.egenolf@gmail.com>
2020-06-05 15:03:02 +0200

    feature: add mTLS configuration for portal-2-verification-server conn… (#65)
    * feature: add mTLS configuration for portal-2-verification-server connection
    
    * feature: add mTLS configuration for portal-2-verification-server connection

--- src/main/java/app/coronawarn/verification/portal/client/VerificationServerFeignClient.java
+++ src/main/java/app/coronawarn/verification/portal/client/VerificationServerFeignClient.java
@@ -23,7 +23,8 @@ package app.coronawarn.verification.portal.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "cwa-verification-server", url = "${cwa-verification-server.url}", primary = false)
+@FeignClient(name = "cwa-verification-server", configuration = VerificationServerClientConfiguration.class,
+  url = "${cwa-verification-server.url}", primary = false)
 public interface VerificationServerFeignClient extends TeleTanClientSI {
 
 }
