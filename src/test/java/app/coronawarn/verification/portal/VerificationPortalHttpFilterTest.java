2305aacbe04287efda20dd1560c0f4211efe73a0
Felix Dittrich <Felix.Dittrich@t-systems.com>
2022-04-19 12:24:39 +0200

    Update Copyright File Header

--- src/test/java/app/coronawarn/verification/portal/VerificationPortalHttpFilterTest.java
+++ src/test/java/app/coronawarn/verification/portal/VerificationPortalHttpFilterTest.java
@@ -1,7 +1,7 @@
 /*
  * Corona-Warn-App / cwa-verification-portal
  *
- * (C) 2021, T-Systems International GmbH
+ * (C) 2020 - 2022, T-Systems International GmbH
  *
  * Deutsche Telekom AG and all other contributors /
  * copyright owners license this file to you under the Apache
@@ -18,6 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package app.coronawarn.verification.portal;
 
 
